8ed744a9b5f75d8da6f305919835065045273504
Глеб Попов <0504051@mail.ru>
2025-01-30 12:07:04 +0300

    change name

--- src/app/data/components/header/header.component.ts
+++ src/app/data/components/header/header.component.ts
@@ -9,10 +9,10 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class HeaderComponent {
   @Input() logoUrl: string = "https://www.titan2.ru/images/temp/logo__ru.jpg"; // URL логотипа
   @Input() buttonText: string = ''; // Текст кнопки
-  @Output() create: EventEmitter<void> = new EventEmitter<void>(); 
+  @Output() buttonClick: EventEmitter<void> = new EventEmitter<void>(); 
 
   Click(): void {
-    this.create.emit(); // Emit the toggle event
+    this.buttonClick.emit(); // Emit the toggle event
   }
 
 }
